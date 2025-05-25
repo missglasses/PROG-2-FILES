ce9051c16f96af2338e421c6675a185f735faa48
Cindy Lepatan <143421355+missglasses@users.noreply.github.com>
2025-05-25 15:37:16 +0800

    Create problems.h

---
+++ finals/StudentExamGrd-Files/problems.h
@@ -0,0 +1,10 @@
+//READ-ONLY
+#ifndef PROBLEM_H
+#define PROBLEM_H
+#include "functions.h"
+
+void insertLast(Node**, studentRecord, gradeRecords);
+
+Node* populate(studentNode*,gradeNode*);
+
+#endif
