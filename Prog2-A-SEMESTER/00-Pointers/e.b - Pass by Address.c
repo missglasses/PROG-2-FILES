fb2732eae4a518c4559af8c50ffaf617ebba0d54
Cindy Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-08 22:01:42 +0800

    Update e.b - Pass by Address.c

--- Prog2-A-SEMESTER/00-Pointers/e.b - Pass by Address.c
+++ Prog2-A-SEMESTER/00-Pointers/e.b - Pass by Address.c
@@ -1,1 +1,19 @@
+#include <st
 
+// void changeVal(numero)
+void changeVal(int *numero)
+{
+    *numero = 5;
+}
+
+int main()
+{
+
+    int numero = 3;
+    // changeVal(numero);
+    changeVal(&numero);
+
+    printf("%d", numero); //prints 5
+
+    return 0;
+}
