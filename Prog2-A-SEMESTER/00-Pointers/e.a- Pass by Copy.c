0c784c842b1748d1c5985c3df5b5c6dc9daa9efa
Cindy Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-08 21:59:40 +0800

    Update e.a- Pass by Copy.c

--- Prog2-A-SEMESTER/00-Pointers/e.a- Pass by Copy.c
+++ Prog2-A-SEMESTER/00-Pointers/e.a- Pass by Copy.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+//THE VALUE IN MAIN REMAINS THE SAME
+void changeVal(numero)
+{
+    numero = 5;
+}
 
+int main()
+{
+
+    int numero = 3;
+    changeVal(numero);
+
+    printf("%d", numero); //prints 3
+
+    return 0;
+}
