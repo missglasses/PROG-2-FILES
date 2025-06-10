5f49136344bf1d6e1a42ab5f26c0e680c8993c4e
Cindy Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-10 18:26:58 +0800

    Create Bubble Sort.c

---
+++ Prog2-A-SEMESTER/03 - Elemetary Sorting Algo/Bubble Sort.c
@@ -0,0 +1,40 @@
+// Optimized implementation of Bubble sort
+#include <stdio.h>
+
+void swap(int* xp, int* yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubbleSort(int arr[], int n){
+    int i, j;
+    int swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;  // 0 means false
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1;  // 1 means true
+            }
+        }
+        
+        if (swapped == 0)
+            break;
+    }
+}
+
+void printArray(int arr[], int size){
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
+
+int main(){
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
