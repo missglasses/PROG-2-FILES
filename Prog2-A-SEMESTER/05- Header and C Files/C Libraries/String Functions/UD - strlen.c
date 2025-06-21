c96303dc4780ecb8e228dcbf9d97d8972c50c917
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-22 07:23:43 +0800

    Update UD - strlen.c

--- Prog2-A-SEMESTER/05- Header and C Files/C Libraries/String Functions/UD - strlen.c
+++ Prog2-A-SEMESTER/05- Header and C Files/C Libraries/String Functions/UD - strlen.c
@@ -3,12 +3,26 @@
 
 int myStrlen(char str[]){
     
-    int i;
+    int i=0;
     while (str[i]!='\0'){
         i++;
     }
     return i;
     
+}
+
+int skipSpace(char str[]){
+    int i=0, count=0,flag=0;
+    while (str[i]!='\0'){
+        if(str[i]==' '||str[i]=='\n'||str[i]=='\t'){
+          flag=0;
+        } else if (flag==0){
+            flag =1;
+            count++;
+        }
+        i++;
+    }
+     return count;
 }
 
 
@@ -17,8 +31,11 @@ int main() {
     char str[50]={"Let's pass prog 2!"}; //Length: 18
     
     int len =myStrlen(str);
+    int len2 = skipSpace(str);
     
-    printf("Length: %d\n", len);
+    
+    printf("Full Length: %d\n", len);
+    printf("Word Count: %d\n", len2);
 
     return 0;
 }
