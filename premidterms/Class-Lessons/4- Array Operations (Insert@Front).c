e235f52be20de78f23214ad112e354aeed024d7f
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-23 20:28:25 +0800

    Update 4- Array Operations (Insert@Front).c

--- premidterms/Class-Lessons/4- Array Operations (Insert@Front).c
+++ premidterms/Class-Lessons/4- Array Operations (Insert@Front).c
@@ -17,7 +17,7 @@ int main() {
     insertFront(myArray,&n,40);
     insertFront(myArray,&n,50);
     int i;
-    for(i=0; i<MAX_SIZE; ++i){
+    for(i=0; i<n; ++i){
         printf("%d ", myArray[i]); //50 40 30 20 10 
         
     }
