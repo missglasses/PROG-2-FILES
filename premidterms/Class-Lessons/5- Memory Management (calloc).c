0f0e8626c8ac11f7b2e43fa1a19d924f87d6a8ce
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-25 08:17:26 +0800

    Update 5- Memory Management (calloc).c

--- premidterms/Class-Lessons/5- Memory Management (calloc).c
+++ premidterms/Class-Lessons/5- Memory Management (calloc).c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int size=5;
+    int myArray;
+
+    int *myCalloc = calloc(sizeof(int), 5);
+    
+    if(myCalloc==NULL){
+        printf("Failed to allocate");
+        exit(1);
+    }
+    
+    for (int i = 0; i < size; i++) {
+        printf("%d ", myCalloc[i]);
+    }
+    
+    free(myCalloc);
+    
+    return 0;
+}
