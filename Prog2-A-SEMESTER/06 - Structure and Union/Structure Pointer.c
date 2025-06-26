b07bb9c9888a21c17572b1f4e1c30d3c881af5b2
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-27 06:30:15 +0800

    Update Structure Pointer.c

--- Prog2-A-SEMESTER/06 - Structure and Union/Structure Pointer.c
+++ Prog2-A-SEMESTER/06 - Structure and Union/Structure Pointer.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+// STRUCT POINTER APPLICATION
+//->  
+struct Car
+{
+    int id;
+    char brand[20];
+    char model[20];
+    char color[20];
+};
+
+int main()
+{
+
+    struct Car car = {00, "Mazda", "Miata", "Black"}; // initial values
+
+    struct Car *carPtr = &car; //this is the structure pointer
+
+    printf("Car Details:\n");
+    printf("ID: %d\n", carPtr->id);
+    printf("Brand: %s\n", carPtr->brand);
+    printf("Model: %s\n", carPtr->model);
+    printf("Color: %s\n", carPtr->color);
+
+    return 0;
+}
