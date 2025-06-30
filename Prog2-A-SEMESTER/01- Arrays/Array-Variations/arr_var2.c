8c50bbf97ee4a21bfa1e507d4369a0bc8b27de8b
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-30 14:37:16 +0800

    Update arr_var2.c

--- Prog2-A-SEMESTER/01- Arrays/Array-Variations/arr_var2.c
+++ Prog2-A-SEMESTER/01- Arrays/Array-Variations/arr_var2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+//variation #2
 
+int main() {
+    int data[5]={4,7,10,-1}; //-1 as the delimiter
+
+    
+    for(int i=0; data[i]!=-1; i++){
+          printf("%d ", data[i]);
+    }
+    
+    return 0;
+}
