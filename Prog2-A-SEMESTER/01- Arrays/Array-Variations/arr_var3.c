e08fcd0b761c392d5a16959c41f928d81a1ba8fb
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-30 14:55:31 +0800

    Update arr_var3.c

--- Prog2-A-SEMESTER/01- Arrays/Array-Variations/arr_var3.c
+++ Prog2-A-SEMESTER/01- Arrays/Array-Variations/arr_var3.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+//variation #3
 
+int main() {
+    int data[5]={3,4,7,10}; //count is the first element 
+    
+    //before adding:
+     printf("Old array: \n");
+    for(int i=0; i<5 ; i++){
+          printf("%d ", data[i]);
+    }
+     printf("\n");
+      printf("Added an element: \n");
+    if (data[0]<4){ //while data[0] serves as count
+    data[data[0]+1]=15; //means <-- data[count+1]=item 
+    data[0]++; //increase count
+    } else{
+        printf("Full na!\n");
+    }
+    
+    
+    //after adding:
+   for(int i=0; i<5 ; i++){
+          printf("%d ", data[i]);
+    }
+    return 0;
+}
