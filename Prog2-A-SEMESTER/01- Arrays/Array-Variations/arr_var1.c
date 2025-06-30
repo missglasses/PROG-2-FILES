8f1311b3d18ce928d4bfdaf6b08da28490c06459
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-30 14:31:49 +0800

    Update arr_var1.c

--- Prog2-A-SEMESTER/01- Arrays/Array-Variations/arr_var1.c
+++ Prog2-A-SEMESTER/01- Arrays/Array-Variations/arr_var1.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+//VARIATION #1
+int main() {
+    int data[5]={4,7,5};
+    int count =3;
+    
+    for(int i=0; i<count; i++){
+          printf("%d", data[i]);
+      if (i<count-1){
+            printf(",");
+      }
+    }
+    
+    return 0;
+}
