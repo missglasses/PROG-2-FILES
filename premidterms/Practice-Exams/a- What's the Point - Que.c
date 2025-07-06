07e6dbaa582139598f3c3f07d3c590dcb18764ad
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-06 17:03:04 +0800

    Create a- What's the Point - Que.c

---
+++ premidterms/Practice-Exams/a- What's the Point - Que.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// add your structure here
+
+
+// add your function prototypes here
+
+
+void main() {
+    int x, y;
+
+    printf("Enter x - coordinate: ");
+    scanf("%d", &x);
+    printf("Enter y - coordinate: ");
+    scanf("%d", &y);
+
+    //insert code here
+
+}
+
+// add your function definitions here
