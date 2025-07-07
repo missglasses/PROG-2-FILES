cabecb1e724df9ef868ac3014e515632515c1436
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-08 07:33:30 +0800

    Update Remove Space- Ans.c

--- premidterms/Practice-Exams/Remove Space- Ans.c
+++ premidterms/Practice-Exams/Remove Space- Ans.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+void removeSpace(char str[]){
+    
+    int i=0, j=0;
+    
+    while(str[i]!='\0'){
+        if(str[i]!=' ' && str[i]!='\t' && str[i]!='\n'){
+            str[j]=str[i];//to keep char 
+         j++;
+        }
+       i++; 
+    }
+    //add null term
+    str[j]='\0';    
+}
+
+
+
+int main (){
+
+    char str[100];
+    //char str2[100];
+    printf("Enter a string: ");
+    scanf(" %[^\n]", str); 
+    
+    removeSpace(str);
+    
+    //printf("\nModified string: %s", str2);
+    printf("Modified string: %s", str);
+    
+
+    
+    return 0;
+}
