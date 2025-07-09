bff058aa303f3f9eb2084146a94053e41a7e8447
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-09 14:51:49 +0800

    Create LL - Insert First.c

---
+++ prefinals/adt-list: linked-list/LL - Insert First.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
+    int data;
+    struct node *next;
+
+}NodeType, *NodePtr; 
+
+void insertFront(NodePtr *list, int data);
+
+void display(NodePtr list);
+
+int main (){
+    NodePtr myList =NULL; 
+    NodePtr *pList = &myList;
+
+    display(myList);
+    insertFront(&myList, 66);
+    //you can also:
+    //insertFront(pList, 66);
+    display(myList);
+    insertFront(&myList, 45);
+    display(myList);
+    insertFront(&myList, 89);
+    display(myList);
+    
+
+    return 0;
+}
+
+void insertFront(NodePtr *list, int data){
+
+    NodePtr temp = malloc(sizeof(NodeType)); //if it doesn't work, use struct node 
+    
+    if (temp){
+        temp->data=data;
+        temp->next=*list;
+        *list = temp;
+    }
+
+
+}
+
+void display(NodePtr list){
+
+    printf("{");
+    while(list){
+        printf("%d", list->data);
+        if(list->next!=NULL){
+            printf("->");
+        }
+        list = list->next;
+    }
+      printf("}\n");
+
+}  
+/* OUTPUT: 
+{}
+{66}
+{45->66}
+{89->45->66}*/
