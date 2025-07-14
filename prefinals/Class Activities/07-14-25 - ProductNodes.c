47b97f786b984fb498e1f74e456c90f9b3770ebf
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-14 14:38:34 +0800

    Update and rename 07-14-25 - ProductNode.c to 07-14-25 - ProductNodes.c

--- prefinals/Class Activities/07-14-25 - ProductNodes.c
+++ prefinals/Class Activities/07-14-25 - ProductNodes.c
@@ -36,6 +36,14 @@ void displayProducts(ProdList list);
 
 int main (){
 
+     char *months={"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    createDate(31,months[1],2025);
+    createProduct(001, "Tiramisu", createDate(8, months[1],2025), 15, 2995.0);
+    createProduct(002, "Red Velvet", createDate(7, months[1], 2025), 12,1695.0);
+    createProduct(003, " Velvet", createDate(7, months[1], 2025), 12, 1695.0);
+    createProduct(004, "Fruit Festival", createDate(7, months[1], 2025), 12, 2250.0);
+
 
 
     return 0;
@@ -44,21 +52,37 @@ int main (){
 
 Date createDate(int d, int m, int y){
 
+    Date petsa;
+    petsa.day=d;
+    petsa.month=m;
+    petsa.year=y;
+
 }
 void displayDate(Date d){
     
 }
 Product createProduct(int id, String name, Date exp, int qty, float price){
+
+    Product prod;
+    prod.id = id;
+    prod.name = name;
+    prod.exp = exp;
+    prod.qty= qty; 
+    prod.price=price;
     
+
 }
 bool updateProductQty(ProdList *list, int id, int newQty){
     
+    if (list->id==id){
+        list->prodQty=prodQty;
+    }
 }
 void displayProduct (Product p){
     
 }
 void initProductList (ProdList *list){
-    
+    list[0] = {000, "Carrot Cake", Date(9, months[1], 2025), 19, 1995.0};
 }
 ProdList createProductList(){
     
