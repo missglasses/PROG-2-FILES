484b28101f037160eaef13bb1d9681e6755d765b
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-16 12:51:48 +0800

    Create functions.c

---
+++ Prog2-A-SEMESTER/08 - ADT List: Linked List/Activities/WHY ARE THEY MULTIPLYING/functions.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "functions.h"
+
+// Function to create a new node with given data
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to append a new node at the end of the linked list
+void append(struct Node** head_ref, int new_data) {
+    struct Node* new_node = createNode(new_data);
+    struct Node* last = *head_ref;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    last->next = new_node;
+}
+
+// Function to print the linked list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
